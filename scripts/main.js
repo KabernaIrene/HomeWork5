2a5378ee6e87408c5e13763873cff19803a8a42b
Kaberna Irene <irinakabernaa@gmail.com>
2023-10-18 11:21:51 +0300

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+const hours = window.prompt ("Введіть, будь ласка, кількість годин", 0);
+
+const seconds = hours * 3600;
+
+
+
+
+const result = alert(`В зазначеній кількості годин ${seconds} секунд`);
+
+
